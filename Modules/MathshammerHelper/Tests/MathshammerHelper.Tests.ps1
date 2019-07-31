2adff91233d736759596924dd578d13a11c1cdeb
Sean Gray <sg@ultima.bigyak.io>
2019-07-31 23:37:38 +0100

    try the psm1 file?

--- Modules/MathshammerHelper/Tests/MathshammerHelper.Tests.ps1
+++ Modules/MathshammerHelper/Tests/MathshammerHelper.Tests.ps1
@@ -1,4 +1,4 @@
-#Import-Module "$($env:Build_SourcesDirectory)/Modules/MathshammerHelper/MathshammerHelper.psd1"
+Import-Module "$($env:Build_SourcesDirectory)/Modules/MathshammerHelper/MathshammerHelper.psm1"
 
 # Some detaults
 $bs = 3
