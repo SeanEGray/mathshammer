728ec11def3ca85326461153b52e2e682d74966b
Sean Gray <sg@ultima.bigyak.io>
2019-08-04 18:25:35 +0100

    Don't scriptanalyse yet

--- Modules/MathshammerHelper/Tests/runTests.ps1
+++ Modules/MathshammerHelper/Tests/runTests.ps1
@@ -1,5 +1,5 @@
 Install-Module PSScriptAnalyzer
-Invoke-ScriptAnalyzer 
+#Invoke-ScriptAnalyzer 
 
 Install-Module Pester -Force
 Invoke-Pester -Script *.Tests.ps1 -OutputFormat NUnitXml -OutputFile 'TEST-HelperModule.xml'
