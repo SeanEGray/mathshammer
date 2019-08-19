f4144ce045b5d35dd3684019999f684f99249a98
Sean Gray <sg@ultima.bigyak.io>
2019-08-19 18:40:44 +0100

    Refactor into dedicated files

--- Modules/MathshammerHelper/Tests/runTests.ps1
+++ Modules/MathshammerHelper/Tests/runTests.ps1
@@ -1,15 +1,3 @@
-Install-Module PSScriptAnalyzer -Force
 Install-Module Pester -Force
-
-$rules = Get-ScriptAnalyzerRule
-
-Describe 'PowerShell ScriptAnalyzer tests' {
-    foreach ($rule in $rules) {
-        It “passes the PSScriptAnalyzer Rule $rule“ {
-            (Invoke-ScriptAnalyzer -Path '../MathsHammerHelper.psm1' -IncludeRule $rule.RuleName ).Count | Should Be 0
-        }
-    }
-}
-
 
 Invoke-Pester -Script *.Tests.ps1 -OutputFormat NUnitXml -OutputFile 'TEST-HelperModule.xml'
