d2a6fd27bb9870433a63771e8f332f4c9e5a40e1
Sean Gray <sg@ultima.bigyak.io>
2019-08-19 18:34:01 +0100

    copypaste apostrophes

--- Modules/MathshammerHelper/Tests/runTests.ps1
+++ Modules/MathshammerHelper/Tests/runTests.ps1
@@ -2,7 +2,7 @@ Install-Module PSScriptAnalyzer -Force
 
 $rules = Get-ScriptAnalyzerRule
 
-Describe ‘PowerShell ScriptAnalyzer tests’ {
+Describe 'PowerShell ScriptAnalyzer tests' {
     foreach ($rule in $rules) {
         It “passes the PSScriptAnalyzer Rule $rule“ {
             (Invoke-ScriptAnalyzer -Path '../MathsHammerHelper.psm1' -IncludeRule $rule.RuleName ).Count | Should Be 0
