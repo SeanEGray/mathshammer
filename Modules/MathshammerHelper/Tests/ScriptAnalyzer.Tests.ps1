681235c4bb15642d8822f6543b4130b1f99c3330
Sean Gray <sg@ultima.bigyak.io>
2019-08-19 18:58:03 +0100

    Correct path

--- Modules/MathshammerHelper/Tests/ScriptAnalyzer.Tests.ps1
+++ Modules/MathshammerHelper/Tests/ScriptAnalyzer.Tests.ps1
@@ -1,6 +1,6 @@
 Install-Module PSScriptAnalyzer -Force
 
-$scripts = Get-ChildItem '/' -Filter '*.ps1' -Recurse | Where-Object {$_.name -NotMatch 'Tests.ps1'}
+$scripts = Get-ChildItem '../' -Filter '*.ps1' -Recurse | Where-Object {$_.name -NotMatch 'Tests.ps1'}
 $rules = Get-ScriptAnalyzerRule
 Describe 'PSSA Tests' {
     foreach ($script in $scripts) {
