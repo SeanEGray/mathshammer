8866877e769cf8f89e1a403094044b8005b92145
Sean Gray <sg@ultima.bigyak.io>
2019-08-19 19:55:04 +0100

    Parameter comments

--- Modules/MathshammerHelper/Public/Get-HammeredMaths.ps1
+++ Modules/MathshammerHelper/Public/Get-HammeredMaths.ps1
@@ -1,50 +1,62 @@
 function Hammer-Maths {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseApprovedVerbs", "")] # Executive decision that Hammer should be an approved verb
     Param (
+        # Ballistic Skill
         [Parameter(Mandatory=$true)]
         [ValidateRange('Positive')]
         [int]
         $bs,
+        # Number of attacking models
         [Parameter(Mandatory=$true)]
         [ValidateRange('Positive')]
         [int]
         $models,
+        # Number of attacks per model
         [Parameter(Mandatory=$true)]
         [ValidateRange('Positive')]
         [int]
         $attacks,
+        # Strength
         [Parameter(Mandatory=$true)]
         [ValidateRange('Positive')]
         [int]
         $s,
+        # Toughness
         [Parameter(Mandatory=$true)]
         [ValidateRange('Positive')]
         [int]
         $t,
+        # Armour-piercing value
         [Parameter(Mandatory=$false)]
         [ValidateRange('NonNegative')]
         [int]
         $ap = 0,
+        # Armour save
         [Parameter(Mandatory=$false)]
         [ValidateRange('NonNegative')]
         [int]
         $save = 0,
+        # Invulnerable save
         [Parameter(Mandatory=$false)]
         [ValidateRange('NonNegative')]
         [int]
         $invulnerableSave = 0,
+        # Damage per unsaved wound
         [Parameter(Mandatory=$true)]
         [ValidateRange('Positive')]
         [int]
         $damage,
+        # Wounds per target model
         [Parameter(Mandatory=$true)]
         [ValidateRange('Positive')]
         [int]
         $targetWounds,
+        # Number of models in the target unit
         [Parameter(Mandatory=$true)]
         [ValidateRange(1,50)]
         [int]
         $targetModels,
+        # Leadership of the target unit
         [Parameter(Mandatory=$true)]
         [ValidateRange('Positive')]
         [int]
