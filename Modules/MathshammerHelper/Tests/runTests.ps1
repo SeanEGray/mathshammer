1ebde85635d626293a8080bbb5a6b66c499d1298
Sean Gray <sg@ultima.bigyak.io>
2019-08-19 18:19:29 +0100

    Add pssa tests

--- Modules/MathshammerHelper/Tests/runTests.ps1
+++ Modules/MathshammerHelper/Tests/runTests.ps1
@@ -1,5 +1,14 @@
 Install-Module PSScriptAnalyzer
-#Invoke-ScriptAnalyzer 
+
+$rules = Get-ScriptAnalyzerRule
+
+Describe ‘PowerShell ScriptAnalyzer tests’ {
+    foreach ($rule in $rules) {
+        It “passes the PSScriptAnalyzer Rule $rule“ {
+            (Invoke-ScriptAnalyzer -Path '../MathsHammerHelper.psm1' -IncludeRule $rule.RuleName ).Count | Should Be 0
+        }
+    }
+}
 
 Install-Module Pester -Force
 Invoke-Pester -Script *.Tests.ps1 -OutputFormat NUnitXml -OutputFile 'TEST-HelperModule.xml'
