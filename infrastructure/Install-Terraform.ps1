da92ba57d5f959738d6adead148e2fc63abb051e
Sean Gray <sg@ultima.bigyak.io>
2019-08-29 21:41:00 +0100

    Add Install-Terraform task

---
+++ infrastructure/Install-Terraform.ps1
@@ -0,0 +1,12 @@
+Param (
+    # Version of terraform to install
+    [Parameter(Mandatory = $false)]
+    [String]
+    $Version = '0.12.7'
+)
+
+$downloadUrl = "https://releases.hashicorp.com/terraform/$version/terraform_$($version)_linux_amd64.zip"
+
+Invoke-WebRequest -Uri $downloadUrl -OutFile "$($env:temp)/terraform.zip"
+
+Expand-Archive -Path "$($env:temp)/terraform.zip" -DestinationPath './' -Force
