20eeb27a272aca8d345cccd113d7e349c38b4463
Sean Gray <sg@ultima.bigyak.io>
2019-11-13 21:57:55 +0000

    Fix content type

--- infrastructure/terraform/web.tf
+++ infrastructure/terraform/web.tf
@@ -1,30 +1,31 @@
 resource "azurerm_storage_account" "web" {
-    name                     = "mathshammerdevweb"
-    location            = "${var.location}"
-    resource_group_name = "${azurerm_resource_group.mathshammer.name}"
-    account_kind = "StorageV2"
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
-    # custom_domain = "mathshammer.bigyak.io"
+  name                      = "mathshammerdevweb"
+  location                  = "${var.location}"
+  resource_group_name       = "${azurerm_resource_group.mathshammer.name}"
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  enable_https_traffic_only = true
+  # custom_domain = "mathshammer.bigyak.io"
 
-    tags = {
-        environment = "${var.environment}"
-        application = "mathshammer"
-        provisioner = "terraform"
-    }
+  tags = {
+    environment = "${var.environment}"
+    application = "mathshammer"
+    provisioner = "terraform"
+  }
 }
 
 resource "azurerm_storage_container" "web" {
-    name                  = "$web"
-    storage_account_name  = "${azurerm_storage_account.web.name}"
-    container_access_type = "blob"
+  name                  = "$web"
+  storage_account_name  = "${azurerm_storage_account.web.name}"
+  container_access_type = "blob"
 }
 
 resource "azurerm_storage_blob" "indexhtml" {
-    name                   = "index.html"
-    storage_account_name   = "${azurerm_storage_account.web.name}"
-    storage_container_name = "${azurerm_storage_container.web.name}"
-    type                   = "Block"
-    source                 = "index.html"
+  name                   = "index.html"
+  storage_account_name   = "${azurerm_storage_account.web.name}"
+  storage_container_name = "${azurerm_storage_container.web.name}"
+  type                   = "Block"
+  source                 = "index.html"
+  content_type           = "text/html"
 }
